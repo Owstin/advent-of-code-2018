cf3de8037054a70171374a39319a8edd70d8b5e8
Owstin <owstingreen@gmail.com>
2018-12-07 19:56:44 -0600

    day 1 part 2

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -2,11 +2,23 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 const inputFile = 'input.txt';
-const data = readFileSync(join(__dirname, inputFile), 'utf8');
-
-const sum = data
-  .trim()
+let data = readFileSync(join(__dirname, inputFile), 'utf8')
   .split('\n')
-  .reduce((sum: number, value: string): number => sum += parseInt(value), 0);
+  .map(val => parseInt(val));
 
+// part 1
+const sum = data.reduce((sum, value) => sum += value, 0);
 console.log(sum);
+
+// part 2
+const previousFreqs: number[] = [];
+let currentFreq = 0;
+for (let i = 0, n = data.length;; i++) {
+  currentFreq += data[i % n];
+  if (previousFreqs.indexOf(currentFreq) !== -1) {
+    console.log(currentFreq)
+    break;
+  } else {
+    previousFreqs.push(currentFreq);
+  }
+}
